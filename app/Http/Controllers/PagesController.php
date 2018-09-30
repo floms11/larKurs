332b46238f276aa614a5cbf1516fac598d5bc03f
Floms <jeka.floms@gmail.com>
2018-09-30 17:01:19 +0300

    Code optimization v2

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Users;
 
-<<<<<<< HEAD
 class ActionKurs
 {
     const MAX_RESULT = 5; // Maximum number of points received
@@ -47,53 +46,31 @@ class ActionKurs
         $usersData = Users::where('id', $this->userID)->get();
         return $usersData[0];
     }
-=======
-class PagesController extends Controller
-{
-    const MAX_RESULT = 5; // Maximum number of points received
-    const MIN_VIDEO_LENGHT = 55; // Length of video
->>>>>>> a6ad60745b6b8371efbc74ee6b520a66499e573d
     /**
      * Change the page to random
      * @param  integer $userID          User id
      * @param  Request $request         Request
      * @return string                   Page Name
      */
-<<<<<<< HEAD
     public function getRandomPage()
-=======
-    private function getRandomPage($userID, Request $request)
->>>>>>> a6ad60745b6b8371efbc74ee6b520a66499e573d
     {
         $pages = ['text', 'summ', 'languages', 'days', 'video'];
         $pagesRandom = [];
         $page = 'finish';
 
         foreach ($pages as $p) {
-<<<<<<< HEAD
             if (!$this->request->session()->get($p, false)) {
-=======
-            if (!$request->session()->get($p, false)) {
->>>>>>> a6ad60745b6b8371efbc74ee6b520a66499e573d
                 $pagesRandom[] = $p;
             }
         }
         $count = count($pagesRandom);
         if ($count > 0) {
             $page = $pagesRandom[rand(0, $count - 1)];
-<<<<<<< HEAD
             $this->request->session()->put($page, true);
         } else {
             Users::where('id', $this->userID)->update(['endTime' => time()]);
         }
         $this->request->session()->put('current', $page);
-=======
-            $request->session()->put($page, true);
-        } else {
-            Users::where('id', $userID)->update(['endTime' => time()]);
-        }
-        $request->session()->put('current', $page);
->>>>>>> a6ad60745b6b8371efbc74ee6b520a66499e573d
         return $page;
     }
     /**
@@ -101,15 +78,9 @@ class PagesController extends Controller
      * @param  Request $request         Request
      * @return string                   Current page
      */
-<<<<<<< HEAD
     public function getCurrentPage()
     {
         return $this->currentPage;
-=======
-    private function getCurrentPage(Request $request)
-    {
-        return $request->session()->get('current', '');
->>>>>>> a6ad60745b6b8371efbc74ee6b520a66499e573d
     }
     /**
      * Returns a page, or redirects to the current one
@@ -118,38 +89,22 @@ class PagesController extends Controller
      * @param  array [$params = null]   Array of Variables
      * @return string                   Returns a page, or redirects to the current one
      */
-<<<<<<< HEAD
     public function getPage($page, $params = null)
     {
         $cp = $this->currentPage;
-=======
-    private function getPage($page, Request $request, $params = null)
-    {
-        $cp = self::getCurrentPage($request);
->>>>>>> a6ad60745b6b8371efbc74ee6b520a66499e573d
         if ($page !== $cp) {
             return redirect('/larKurs/' . $cp);
         } else {
             return ($params == null) ? view($page) : view($page, $params);
         }
-<<<<<<< HEAD
-=======
-        getCurrentPage
->>>>>>> a6ad60745b6b8371efbc74ee6b520a66499e573d
     }
     /**
      * Will score the user
      * @param integer $userID User id
      */
-<<<<<<< HEAD
     public function plusResult()
     {
         $res = Users::where('id', $this->userID)->get()[0]->result;
-=======
-    private function plusResult($userID)
-    {
-        $res = Users::where('id', $userID)->get()[0]->result;
->>>>>>> a6ad60745b6b8371efbc74ee6b520a66499e573d
         $res += 1;
         if ($res < 0) {
             $res = 0;
@@ -157,7 +112,6 @@ class PagesController extends Controller
         if ($res > self::MAX_RESULT) {
             $res = self::MAX_RESULT;
         }
-<<<<<<< HEAD
         Users::where('id', $this->userID)->update(['result' => $res]);
     }
     public function pageCheck($page)
@@ -171,49 +125,12 @@ class PagesController extends Controller
                 $resInput = $this->request->input('summ');
                 if ($resInput == $res) {
                     $this->plusResult();
-=======
-        Users::where('id', $userID)->update(['result' => $res]);
-    }
-    /**
-     * Processing of basic actions
-     * @param  Request $request         Request
-     * @param  string $page             Page name
-     * @return string                   Redirect last page
-     */
-    public function triger(Request $request, $page)
-    {
-        $currentPage = self::getCurrentPage($request);
-        $userID = $request->session()->get('id', 0);
-
-        // If the user is not authorized
-        if ($userID < 1) {
-            return redirect('larKurs/');
-        }
-        // If the request is not for the current "operation"
-        if ($currentPage !== $page) {
-            return redirect('/larKurs/' . $currentPage);
-        }
-
-        switch ($page) {
-            case 'text':
-                self::plusResult($userID);
-                break;
-            case 'summ': // Give a score if the sum of the numbers is correct
-                $res = $request->session()->get('summRes');
-                $resInput = $request->input('summ');
-                if ($resInput == $res) {
-                    self::plusResult($userID);
->>>>>>> a6ad60745b6b8371efbc74ee6b520a66499e573d
                 }
                 break;
             case 'languages': // We determine which languages the user has chosen, and perform the actions we need (paragraph 4.4)
                 $resInput = array();
                 for ($i = 1; $i < 6; $i++) {
-<<<<<<< HEAD
                     $resInput[] = $this->request->input('v' . $i);
-=======
-                    $resInput[] = $request->input('v' . $i);
->>>>>>> a6ad60745b6b8371efbc74ee6b520a66499e573d
                 }
                 $count = 0;
                 $vb = false;
@@ -226,7 +143,6 @@ class PagesController extends Controller
                     }
                 }
                 if ($count > 0 && !$vb) {
-<<<<<<< HEAD
                     $this->plusResult();
                 }
                 break;
@@ -330,27 +246,6 @@ class PagesController extends Controller
         $act->pageCheck($page);
         
         return redirect('/larKurs/' . $act->getRandomPage());
-=======
-                    self::plusResult($userID);
-                }
-                break;
-            case 'days': // We are verifying the current day of the week
-                $res = $request->session()->get('daysTrue');
-                $resInput = $request->input('value');
-                if ($resInput == $res) {
-                    self::plusResult($userID);
-                }
-                break;
-            case 'video': // Check the video until the end
-                $startTime = $request->session()->get('videoStartTime');
-                $resInput = $request->input('lenght');
-                if (time() - $startTime >= $resInput && $resInput > self::MIN_VIDEO_LENGHT) {
-                    self::plusResult($userID);
-                }
-                break;
-        }
-        return redirect('/larKurs/' . self::getRandomPage($userID, $request));
->>>>>>> a6ad60745b6b8371efbc74ee6b520a66499e573d
     }
     /**
      * We register for the passage of the "course"
@@ -359,14 +254,10 @@ class PagesController extends Controller
      */
     public function trigerReg(Request $request)
     {
-<<<<<<< HEAD
         $act = new ActionKurs($request);
 
         $userID = $act->getUserID();
 
-=======
-        $userID = $request->session()->get('id', 0);
->>>>>>> a6ad60745b6b8371efbc74ee6b520a66499e573d
         // We register the user
         if ($userID < 1) {
             $this->validate($request, [
@@ -377,22 +268,12 @@ class PagesController extends Controller
                 // No name entered
                 return redirect('/larKurs/');
             }
-<<<<<<< HEAD
             $act->regUser($name);
             // Registered, now start the course
             return redirect('/larKurs/' . $act->getRandomPage());
         }
         // User is already registered, redirected to the last page
         return redirect('/larKurs/' . $act->getCurrentPage());
-=======
-            $id = Users::insertGetId(['name' => $name, 'startTime' => time()]);
-            $userID = $request->session()->put('id', $id);
-            // Registered, now start the course
-            return redirect('/larKurs/' . self::getRandomPage($userID, $request));
-        }
-        // User is already registered, redirected to the last page
-        return redirect('/larKurs/' . self::getCurrentPage($request));
->>>>>>> a6ad60745b6b8371efbc74ee6b520a66499e573d
     }
     /**
      * Controller page "start"
@@ -401,18 +282,10 @@ class PagesController extends Controller
      */
     public function start(Request $request)
     {
-<<<<<<< HEAD
         $act = new ActionKurs($request);
 
         $list = $act->getUsersList();
         $cp = $act->getCurrentPage($request);
-=======
-        $list = Users::where('endTime', '>', 0)
-            ->orderBy('endTime', 'desc')
-            ->take(10)
-            ->get();
-        $cp = self::getCurrentPage($request);
->>>>>>> a6ad60745b6b8371efbc74ee6b520a66499e573d
         if ($cp != '') {
             return redirect('/larKurs/' . $cp);
         }
@@ -425,12 +298,8 @@ class PagesController extends Controller
      */
     public function text(Request $request)
     {
-<<<<<<< HEAD
         $act = new ActionKurs($request);
         return $act->getPage('text');
-=======
-        return self::getPage('text', $request);
->>>>>>> a6ad60745b6b8371efbc74ee6b520a66499e573d
     }
     /**
      * Controller page "summ"
@@ -439,25 +308,11 @@ class PagesController extends Controller
      */
     public function summ(Request $request)
     {
-<<<<<<< HEAD
         $act = new ActionKurs($request);
 
         $dataArray = $act->addSummData();
 
         return $act->getPage('summ', $dataArray);
-=======
-        $dataArray = array();
-        $dataArray['summC1'] = $request->session()->get('summC1', 0);
-        $dataArray['summC2'] = $request->session()->get('summC2', 0);
-        if ($dataArray['summC1'] < 10 || $dataArray['summC1'] > 99 || $dataArray['summC2'] < 10 || $dataArray['summC2'] > 99) {
-            $dataArray['summC1'] = rand(10, 99);
-            $dataArray['summC2'] = rand(10, 99);
-            $request->session()->put('summC1', $dataArray['summC1']);
-            $request->session()->put('summC2', $dataArray['summC2']);
-            $request->session()->put('summRes', $dataArray['summC1'] + $dataArray['summC2']);
-        }
-        return self::getPage('summ', $request, $dataArray);
->>>>>>> a6ad60745b6b8371efbc74ee6b520a66499e573d
     }
     /**
      * Controller page "languages"
@@ -466,13 +321,9 @@ class PagesController extends Controller
      */
     public function languages(Request $request)
     {
-<<<<<<< HEAD
         $act = new ActionKurs($request);
 
         return $act->getPage('languages');
-=======
-        return self::getPage('languages', $request);
->>>>>>> a6ad60745b6b8371efbc74ee6b520a66499e573d
     }
     /**
      * Controller page "days"
@@ -481,7 +332,6 @@ class PagesController extends Controller
      */
     public function days(Request $request)
     {
-<<<<<<< HEAD
         $act = new ActionKurs($request);
 
         $d = $act->addDateData();
@@ -489,43 +339,6 @@ class PagesController extends Controller
         $daysText = $d['daysText'];
         
         return $act->getPage('days', compact('days', 'daysText'));
-=======
-        $daysText = ['Воскресенье', 'Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота'];
-        $days = $request->session()->get('daysData');
-        $dayArray = $request->session()->get('daysTrue');
-        $dayNow = date('w');
-        if (!is_array($days) || $dayNow != $dayArray) {
-            $days = array();
-            $days[0] = $dayNow;
-
-            // First
-            if ($days[0] <= 0) {
-                $days[1] = 6;
-            } else {
-                $days[1] = $days[0] - 1;
-            }
-
-            // Second
-            if ($days[0] >= 6) {
-                $days[2] = 0;
-            } else {
-                $days[2] = $days[0] + 1;
-            }
-
-            // The third
-            if ($days[2] >= 6) {
-                $days[3] = 0;
-            } else {
-                $days[3] = $days[2] + 1;
-            }
-
-            $request->session()->put('daysTrue', $dayNow);
-            
-            shuffle($days);
-            $request->session()->put('daysData', $days);
-        }
-        return self::getPage('days', $request, compact('days', 'daysText'));
->>>>>>> a6ad60745b6b8371efbc74ee6b520a66499e573d
     }
     /**
      * Controller page "video"
@@ -534,15 +347,10 @@ class PagesController extends Controller
      */
     public function video(Request $request)
     {
-<<<<<<< HEAD
         $act = new ActionKurs($request);
 
         $act->addVideoData();
         return $act->getPage('video');
-=======
-        $request->session()->put('videoStartTime', time());
-        return self::getPage('video', $request);
->>>>>>> a6ad60745b6b8371efbc74ee6b520a66499e573d
     }
     /**
      * Controller page "finish"
@@ -551,7 +359,6 @@ class PagesController extends Controller
      */
     public function finish(Request $request)
     {
-<<<<<<< HEAD
         $act = new ActionKurs($request);
 
         $userID = $act->getUserID();
@@ -560,15 +367,6 @@ class PagesController extends Controller
             return $act->getPage('finish', compact('userData'));
         } else {
             return $act->getPage('finish');
-=======
-        $userID = $request->session()->get('id', 0);
-        if ($userID > 0) {
-            $usersData = Users::where('id', $userID)->get();
-            $userData = $usersData[0];
-            return self::getPage('finish', $request, compact('userData'));
-        } else {
-            return self::getPage('finish', $request);
->>>>>>> a6ad60745b6b8371efbc74ee6b520a66499e573d
         }
     }
     /**
@@ -578,13 +376,9 @@ class PagesController extends Controller
      */
     public function restart(Request $request)
     {
-<<<<<<< HEAD
         $act = new ActionKurs($request);
 
         $act->resetUser();
-=======
-        $request->session()->flush();
->>>>>>> a6ad60745b6b8371efbc74ee6b520a66499e573d
         return redirect('/larKurs');
     }
 }
